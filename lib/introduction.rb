a80f8f8ae3cbd2a20d90a108b0089425c44d3091
Andres Carvajal <jc1993box@hotmail.com>
2019-06-13 19:45:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,3 @@ def introduction(name)
 end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
-  introduction_with_language ("Andres", "Ruby")
-  
